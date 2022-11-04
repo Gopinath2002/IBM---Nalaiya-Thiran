f761e41e62e12160ace4e592ebeafb881955c876
5HR33H4R <shreehar3400k.e@gmail.com>
2022-11-04 23:27:39 +0530

    Added the flash message - sayHello

--- Shreehar_KE/sayHello/app.py
+++ Shreehar_KE/sayHello/app.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template, request, flash
 
 app = Flask(__name__)
+app.secret_key = "Sherlock@221B"
 
 @app.route("/hello")
 def index():
+    flash("What's your name?")
     return render_template("index.html")
